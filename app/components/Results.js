60fe4f16827b546ff6c56c66af749c67df682803
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-07-22 23:51:41 -0600

    Changes for /results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,12 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var queryString = require('query-string');
-var api = require('../utils/api');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
-var Loading = require('./Loading');
+const React = require('react');
+const PropTypes = require('prop-types');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
+const Loading = require('./Loading');
 
-function Profile (props) {
-  var info = props.info;
-
+function Profile ({ info }) {
   return (
     <PlayerPreview username={info.login} avatar={info.avatar_url}>
       <ul className='space-list-items'>
@@ -28,12 +26,12 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired,
 }
 
-function Player (props) {
+function Player ({ label, score, profile }) {
   return (
     <div>
-      <h1 className='header'>{props.label}</h1>
-      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
-      <Profile info={props.profile} />
+      <h1 className='header'>{label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {score}</h3>
+      <Profile info={profile} />
     </div>
   )
 }
@@ -55,36 +53,29 @@ class Results extends React.Component {
     }
   }
   componentDidMount() {
-    var players = queryString.parse(this.props.location.search);
+    const { playerOneName, playerTwoName } = queryString.parse(this.props.location.search);
 
     api.battle([
-      players.playerOneName,
-      players.playerTwoName
-    ]).then(function (players) {
+      playerOneName,
+      playerTwoName
+    ]).then((players) => {
       if (players === null) {
-        return this.setState(function () {
-          return {
-            error: 'Looks like there was an error. Check that both users exist on Github.',
-            loading: false,
-          }
-        });
+        return this.setState(() => ({
+          error: 'Looks like there was an error. Check that both users exist on Github.',
+          loading: false,
+        }))
       }
 
-      this.setState(function () {
-        return {
-          error: null,
-          winner: players[0],
-          loser: players[1],
-          loading: false,
-        }
-      });
-    }.bind(this));
+      this.setState(() => ({
+        error: null,
+        winner: players[0],
+        loser: players[1],
+        loading: false,
+      }));
+    });
   }
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
+    const { error, winner, loser, loading } = this.state;
 
     if (loading === true) {
       return <Loading />
