84a767e22b827d5676274e10cacc9dfc967f8e48
Tyler McGinnis <tylermcginnis33@gmail.com>
2018-08-30 13:43:49 -0600

    Updates for api

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: ['@babel/polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
